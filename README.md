886c5e69f38bf67c21f8554a097a8e4236dfef52
Ruslan <ruslan.aliyev@siliconstack.com.au>
2022-05-17 17:36:10 +0700

    start

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 - https://www.youtube.com/watch?v=sAj6tdVS2cA
 - https://www.youtube.com/watch?v=7Cjb7Xj8fEI
-- https://v2.vuejs.org/v2/guide/reactivity.html
-- https://vuejs.org/guide/quick-start.html#without-build-tools
+- https://medium.com/swlh/vue3-using-ref-or-reactive-88d47c8f6944
+- https://vuejs.org/guide/essentials/reactivity-fundamentals.html
+- https://v2.vuejs.org/v2/guide/reactivity.html (v2)
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <p>{{unreactive_count}}</p>
+  <button @click="unreactive_count++">Increment (Won't work)</button>
+
+  <p>{{count}}</p>
+  <button @click="count++">Increment</button>
+
+  <p>{{double_count}}</p>
+  <button @click="double">Double</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: "App",
+
+  setup() {
+    let unreactive_count = 0;
+    let count = ref(0);
+    let double_count = ref(1);
+
+    function double() {
+      double_count.value *= 2;
+    }
+
+    return {
+      unreactive_count,
+      count,
+      double_count,
+      double,
+    };
+  }
+}
+</script>
